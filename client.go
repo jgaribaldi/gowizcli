b32f560ee11ac8a7173ca6c61210850e05832485
jgaribaldi <julita.gari@gmail.com>
2025-10-02 21:31:57 -0300

    Add private methods for command execution

--- client.go
+++ client.go
@@ -71,37 +71,67 @@ func (c Client) Execute(command Command) error {
 	switch command {
 
 	case Discover:
-		lights, err := c.wiz.Discover()
-		if err != nil {
-			return err
-		}
-		for _, light := range lights {
-			fmt.Printf("Found new light with MAC Address %s and IP Address %s\n", light.MacAddress, light.IpAddress)
-			c.db.Upsert(light)
-		}
+		c.executeDiscover()
+
 	case Show:
-		println("Lights")
-		println("------")
+		c.executeShow()
 
-		lights, err := c.db.FindAll()
-		if err != nil {
-			panic(err)
-		}
-
-		for _, l := range lights {
-			fmt.Printf("ID: %s - MacAddress: %s - IpAddress: %s\n", l.Id, l.MacAddress, l.IpAddress)
-		}
 	case Reset:
-		c.db.Reset()
-		println("Erased all data - run a discovery to populate again")
+		c.executeReset()
 
 	case TurnOn:
+		c.executeTurnOn()
 
 	case TurnOff:
+		c.executeTurnOff()
 
 	default:
 		return fmt.Errorf("unknown command %s", command)
 	}
 
+	return nil
+}
+
+func (c Client) executeDiscover() error {
+	lights, err := c.wiz.Discover()
+	if err != nil {
+		return err
+	}
+	for _, light := range lights {
+		fmt.Printf("Found new light with MAC Address %s and IP Address %s\n", light.MacAddress, light.IpAddress)
+		_, err := c.db.Upsert(light)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (c Client) executeShow() error {
+	fmt.Println("Lights")
+	fmt.Println("------")
+
+	lights, err := c.db.FindAll()
+	if err != nil {
+		return err
+	}
+
+	for _, l := range lights {
+		fmt.Printf("ID: %s - MacAddress: %s - IpAddress: %s\n", l.Id, l.MacAddress, l.IpAddress)
+	}
+	return nil
+}
+
+func (c Client) executeReset() error {
+	c.db.Reset()
+	fmt.Println("Erased all data - run a discovery to populate again")
+	return nil
+}
+
+func (c Client) executeTurnOn() error {
+	return nil
+}
+
+func (c Client) executeTurnOff() error {
 	return nil
 }
