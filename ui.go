1fb86ef72338173b4e97bb2503d566670fca149e
jgaribaldi <julita.gari@gmail.com>
2025-10-16 21:14:19 -0300

    Remove commented old code

--- ui.go
+++ ui.go
@@ -9,55 +9,6 @@ import (
 	"github.com/charmbracelet/lipgloss"
 )
 
-// type model struct {
-// 	cursor      int
-// 	choice      client.Option
-// 	currentView ViewType
-// }
-//
-// func (m model) Init() tea.Cmd {
-// 	return nil
-// }
-//
-// func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-// 	switch msg := msg.(type) {
-// 	case tea.KeyMsg:
-// 		switch msg.String() {
-// 		case "ctrl+c", "q", "esc":
-// 			return m, tea.Quit
-// 		case "enter":
-// 			m.choice = client.Options[m.cursor]
-// 			return m, tea.Quit
-// 		case "down", "j":
-// 			m.cursor++
-// 			if m.cursor >= len(client.Options) {
-// 				m.cursor = 0
-// 			}
-// 		case "up", "k":
-// 			m.cursor--
-// 			if m.cursor < 0 {
-// 				m.cursor = len(client.Options) - 1
-// 			}
-// 		}
-// 	}
-// 	return m, nil
-// }
-//
-// func (m model) View() string {
-// 	s := strings.Builder{}
-// 	s.WriteString("Welcome to gowizcli! A Wiz lights client written in Go. Pick a command:\n\n")
-// 	for i, c := range client.Options {
-// 		if m.cursor == i {
-// 			s.WriteString("[*] ")
-// 		} else {
-// 			s.WriteString("[ ] ")
-// 		}
-// 		s.WriteString(c.Name)
-// 		s.WriteString("\n")
-// 	}
-// 	return s.String()
-// }
-
 type ViewType int
 
 const (
