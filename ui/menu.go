93000f430bf8be4746e6fcd3d73a9c54979c14b5
jgaribaldi <julita.gari@gmail.com>
2025-10-29 20:05:39 -0300

    Add help view for the menu

--- ui/menu.go
+++ ui/menu.go
@@ -3,17 +3,20 @@ package ui
 import (
 	"strings"
 
+	"github.com/charmbracelet/bubbles/help"
 	"github.com/charmbracelet/bubbles/key"
 	tea "github.com/charmbracelet/bubbletea"
 )
 
 type MenuModel struct {
 	cursor int
+	help   help.Model
 }
 
 func NewMenuModel() MenuModel {
 	return MenuModel{
 		cursor: 0,
+		help:   help.New(),
 	}
 }
 
@@ -25,20 +28,20 @@ func (m MenuModel) Update(msg tea.Msg) (MenuModel, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch {
-		case key.Matches(msg, keyMap.Quit):
+		case key.Matches(msg, keys.Quit):
 			return m, tea.Quit
-		case key.Matches(msg, keyMap.Select):
+		case key.Matches(msg, keys.Select):
 			selectedOption := menuOptions[m.cursor]
 
 			return m, func() tea.Msg {
 				return navigateToMsg{view: selectedOption.View}
 			}
-		case key.Matches(msg, keyMap.MoveUp):
+		case key.Matches(msg, keys.MoveUp):
 			m.cursor--
 			if m.cursor < 0 {
 				m.cursor = len(menuOptions) - 1
 			}
-		case key.Matches(msg, keyMap.MoveDown):
+		case key.Matches(msg, keys.MoveDown):
 			m.cursor++
 			if m.cursor >= len(menuOptions) {
 				m.cursor = 0
@@ -61,20 +64,34 @@ func (m MenuModel) View() string {
 		s.WriteString(o.Name)
 		s.WriteString("\n")
 	}
+	s.WriteString("\n\n")
+	s.WriteString(m.help.View(keys))
 
 	return s.String()
 }
 
-var keyMap = struct {
+type keyMap struct {
 	Quit     key.Binding
 	Select   key.Binding
 	MoveUp   key.Binding
 	MoveDown key.Binding
-}{
-	Quit:     key.NewBinding(key.WithKeys("ctrl+c", "q", "esc"), key.WithHelp("ctrl+c / q / esc", "quit program")),
-	Select:   key.NewBinding(key.WithKeys("enter"), key.WithHelp("enter", "select")),
-	MoveUp:   key.NewBinding(key.WithKeys("up", "k"), key.WithHelp("up arrow / k", "move up")),
-	MoveDown: key.NewBinding(key.WithKeys("down", "j"), key.WithHelp("down arrow / j", "move down")),
+}
+
+func (k keyMap) ShortHelp() []key.Binding {
+	return []key.Binding{k.MoveUp, k.MoveDown, k.Select, k.Quit}
+}
+
+func (k keyMap) FullHelp() [][]key.Binding {
+	return [][]key.Binding{
+		{k.MoveUp, k.MoveDown, k.Select, k.Quit},
+	}
+}
+
+var keys = keyMap{
+	Quit:     key.NewBinding(key.WithKeys("ctrl+q"), key.WithHelp("ctrl+q", "Quit program")),
+	Select:   key.NewBinding(key.WithKeys("enter"), key.WithHelp("enter", "Select")),
+	MoveUp:   key.NewBinding(key.WithKeys("up", "k"), key.WithHelp("up arrow / k", "Move up")),
+	MoveDown: key.NewBinding(key.WithKeys("down", "j"), key.WithHelp("down arrow / j", "Move down")),
 }
 
 var menuOptions = []struct {
